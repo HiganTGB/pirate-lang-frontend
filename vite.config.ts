56a4cfcb8ddf651774b63f3d757d1eacbb3b4eb2
HiganTGB <100978108+HiganTGB@users.noreply.github.com>
2025-05-27 15:37:35 +0700

    Add tailwind, alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+import path from "path";
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Mặc định của Vite
+    },
+  },
+});
